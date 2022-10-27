a01fd07452d2aa634a32f0e6b9ab64e88676724c
Claudio-DiGiovanni <digioclaudio97@gmail.com>
2022-10-27 14:32:05 +0200

    Add style & input

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-const arrGroceryList = [
-    "patate",
-    "cetrioli",
-    "zucchine",
-    "cipolle",
-    "pomodori",
-];
+const arrGroceryList = ["elemento 1", "elemento 2", "elemento 3", "elemento 4"];
 
 const eleGroceryList = document.querySelector(".grocery-list");
 
@@ -13,3 +7,21 @@ while (i != arrGroceryList.length) {
     eleGroceryList.innerHTML += `<li>${arrGroceryList[i]}</li>`;
     i++;
 }
+
+const inputGroceryItem = document.querySelector('input');
+const eleGroceryForm = document.querySelector('form');
+
+eleGroceryForm.addEventListener('submit', function(event) {
+	event.preventDefault();
+
+	if (inputGroceryItem.value) {
+		
+		arrGroceryList.push(inputGroceryItem.value);
+
+		
+		eleGroceryList.innerHTML += `<li>${inputGroceryItem.value}</li>`;
+
+		
+		inputGroceryItem.value = '';
+	}
+});
